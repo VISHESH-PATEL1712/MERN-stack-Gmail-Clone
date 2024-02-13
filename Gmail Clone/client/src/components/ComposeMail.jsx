5f19d69f49d8f3813ac172826a44b99be4ccb301
VISHESH PATEL1712 <visheshp39@gmail.com>
2024-02-13 12:44:04 +0530

    updated1

--- Gmail Clone/client/src/components/ComposeMail.jsx
+++ Gmail Clone/client/src/components/ComposeMail.jsx
@@ -1,6 +1,7 @@
 
 
-import { Dialog } from '@mui/material';
+import { Dialog, Box, Typography, styled, InputBase, TextField } from '@mui/material';
+import { Close } from '@mui/icons-material';
 
 const dialogStyle = {
         height: '90%',
@@ -11,16 +12,43 @@ const dialogStyle = {
         borderRadius: '10px 10px 0 0'
 }
 
+const Header = styled(Box) ({
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '10px 15px',
+    background: '#f2f6fc',
+    '& > p': {
+        fontSize: 14,
+        fontWeight: 500
+    }
+});
+
+const RecipientsWrapper = styled(Box) ({
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '0 15px',
+    '& < div': {
+        fontSize: 14,
+        borderBottom: '1px solid #f5f5f5',
+        marginTop:10
+    }
+})
+
 
 const ComposeMail = () => {
     return (
             <Dialog  open={true} PaperProps={{ sx: dialogStyle }}>
-                <Box>
-
-                </Box>
-                <Box>
-
-                </Box>
+                <Header>
+                      <Typography>New Message</Typography>
+                      <Close fontSize="small" />
+                </Header>
+                <RecipientsWrapper>
+                    <InputBase placeholder="Recipient" />
+                    <InputBase placeholder="Subject"/>
+                </RecipientsWrapper>
+                <TextField multiline 
+                           row={20}
+                           sx={{ '& .MuiOutlinedInput-notchedOutline  ': { border: 'none' }}} />
                 <Box>
                     
                 </Box>
